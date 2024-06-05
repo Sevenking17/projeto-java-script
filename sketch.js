66f32ba3b93aab6e0c9b4927adbe51acf38318fd
Sevenking17 <166755156+Sevenking17@users.noreply.github.com>
2024-06-05 13:22:11 -0300

    Create sketch.js
    adicionando o arquivo js

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function draw() {
+ fill(cor)
+ circle(circuloX[0],circuloY[0],50);
+  circle(circuloX[1],circuloY[1],50);
+  
+  circuloX[0]+=random(0,3);
+ circuloX[1]+=random(0,3);
+  
+   circuloY[0]+=random(-3,3);
+ circuloY[1]+=random(-3,3)
+  
+  
+if(mouseIsPressed){
+  cor=color(random(0,255),
+  random(0,255),
+  random(0,255),
+  random(0,100));}
+ }
